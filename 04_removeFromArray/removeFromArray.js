eaf7d794c2334e2d64105f80d063d8edd4106c97
Jesus <jmartingv96@gmail.com>
2022-05-16 19:51:24 -0700

    added some comments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -13,6 +13,15 @@ const removeFromArray = function (arr, ...args) {
     arr.splice(index, 1);
   }
   return arr;
+
+  // A simpler, but more advanced way to do it is to use the 'filter' function,
+  // which basically does what we did with the forEach above.
+
+  // var removeFromArray = function(...args) {
+  //   const array = args[0]
+  //   return array.filter(val => !args.includes(val))
+  // }
+  //
 };
 
 // Do not edit below this line
