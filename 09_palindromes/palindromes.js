0232ce703d78edce68244f4feb4402be24cebcc8
Jesus <jmartingv96@gmail.com>
2022-06-11 16:23:39 -0700

    solved ex 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let newString = str.toLowerCase().replace(/[^a-z]/g, '');
 
+  return newString.split('').reverse().join('') == newString;
 };
 
 // Do not edit below this line
