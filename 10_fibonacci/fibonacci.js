c9b082dab4d78aef70ac5c96da856f7d808ca962
Jesus <jmartingv96@gmail.com>
2022-06-11 16:36:42 -0700

    solved ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num <= 0) {
+    return 'OOPS';
+  }
 
+  let series = 1;
+  let arr = [series];
+
+  for (let i = 0; i < num - 1; i++) {
+    if (arr.length == 1) {
+      arr.push(1);
+    } else {
+      arr.push(arr[i] + arr[i - 1]);
+    }
+  }
+
+  return arr[num - 1];
 };
 
 // Do not edit below this line
